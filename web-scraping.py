8cc2a74e9ac57edac49c473aee147ce588ad1030
Kristin Germeroth <kristin.germeroth@gmail.com>
2019-07-22 21:48:06 -0700

    function to open web pages from text for testing

--- web-scraping.py
+++ web-scraping.py
@@ -1,5 +1,13 @@
+import webbrowser
+
 
 def open_webpages(filename):
 	"""Opens the webpages of the comp set hotels"""
 
-	
+	website_file = open(filename)
+
+	for line in website_file:
+		webbrowser.open(line.rstrip().split('|')[1])
+
+	website_file.close()
+
