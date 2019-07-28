4f7376be6bc002ee5b621457a0e12675c45767c2
Kristin Germeroth <kristin.germeroth@gmail.com>
2019-07-28 12:16:39 -0700

    started code to clean soup for rank

--- web-scraping.py
+++ web-scraping.py
@@ -43,34 +43,41 @@ def use_html_file(file_to_save_to):
 	return soup_object
 
 def get_data_out_of_soup(soup_object):
-	"""Take beautiful soup object and pull out releveant data"""
+	"""Take beautiful soup object and pull out releveant data
+	
+	#review count results
+	>>>reviewcountstring = str(soup_object.find_all("span", class_="reviewCount ui_link level_4"))
+	[<span class="reviewCount ui_link level_4">3,845 reviews</span>]
 
+	"""
+	# parse out ranking and number of hotels in SF
+	rankstring = str(soup_object("span", class_="header_popularity popIndexValidation ui_link level_4"))
+
+	# get the string from the rank through the end
+	rankhalfstring = rankstring[rankstring.index("<b") + 16:] #clean up to slice the main desired portion
 
 	# parse out review count
 	reviewcountstring = str(soup_object.find_all("span", class_="reviewCount ui_link level_4"))
-	#returns [<span class="reviewCount ui_link level_4">3,845 reviews</span>]
+
 	reviewcount = reviewcountstring[43:-16]
 
 	# need to account for commas before converting to int
-	if len(reviewcount) < 4:												#less than 1,000 reviews
+	if len(reviewcount) < 4:															#less than 1,000 reviews
 		reviewcount = int(reviewcount)
 
-	elif len(reviewcount) > 3 and len(reviewcount) < 8:						#1,000 - 999,999 reviews
+	elif len(reviewcount) > 3 and len(reviewcount) < 8:									#1,000 - 999,999 reviews
 		reviewcount = int((reviewcount[:-4] + reviewcount[-3:]))
 
-	else:																	#more 999,999 reviews
+	else:																				#more 999,999 reviews
 		reviewcount = int((reviewcount[:-8] + reviewcount[-7:-4] + reviewcount[-3:]))
 
-# using beautiful soup to find pieces of info
+
 
 # to find the span with avg review score:
 # >>> soup.find_all("span", {"class":"hotels-hotel-review-about-with-photos-Reviews__overallRating--vElGA"})
 # [<span class="hotels-hotel-review-about-with-photos-Reviews__overallRating--vElGA">3.5</span>]
 
-# this will find the rank, but not the number of hotels in SF:
-# >>> soup.find_all("b", class_="rank")
-# [<b class="rank">#136</b>]
-# This finds all of it:
+# this will find the rank and number of hotels in SF:
 # >>> soup("span", class_="header_popularity popIndexValidation ui_link level_4")
 # [<span class="header_popularity popIndexValidation ui_link level_4" onclick="ta.util.cookie.setPIDCookie(15191);"><b class="rank">#136</b> of 241 <a href="/Hotels-g60713-San_Francisco_California-Hotels.html">Hotels in San Francisco</a></span>]
 
