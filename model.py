37974214fe7cec1efe8b03d991e738bcda89fc9e
Kristin Germeroth <kristin.germeroth@gmail.com>
2019-08-12 12:45:22 -0700

    finished defining table classes and tested database creation

--- model.py
+++ model.py
@@ -2,7 +2,7 @@
 
 from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy
+db = SQLAlchemy()
 
 ######################################################
 
@@ -14,7 +14,7 @@ class Hotel(db.Model):
 	hotel_id = db.Column(db.Integer, primary_key=True, autoincrement=True,)
 	hotel_name = db.Column(db.String(100), nullable=False,)
 	hotel_nickname = db.Column(db.String(10), nullable=False,)
-	ta_url = db.Column(db.Text(), nullable=False,)
+	ta_url = db.Column(db.Text, nullable=False,)
 
 	def __repr__(self):
 		return f'<hotel_id={self.hotel_id} hotel_name={self.hotel_name}>'
@@ -38,8 +38,38 @@ class Scrape(db.Model):
 
 	hotel = db.relationship('Hotel', backref='scrapes')
 
+class User(db.Model):
+	"""User model."""
 
+	__tablename__ = 'users'
 
+	user_id = db.Column(db.Integer, primary_key=True, autoincrement=True,)
+	email = db.Column(db.Text, nullable=False,)
+	password = db.Column(db.String, nullable=False,)
+	default_view = db.Column(db.Integer, db.ForeignKey('view.view_id'), nullable=True,)
+
+	def __repr__(self):
+		print(f'<user_id={self.user_id}> default_view={self.default_view}')
+
+	view = db.relationship('View', backref='users')
+
+class View(db.Model):
+	"""View model."""
+
+	__tablename__ = 'view'
+
+	view_id = db.Column(db.Integer, primary_key=True, autoincrement=True,)
+	user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False,)
+	view_name = db.Column(db.String(50), nullable=False,)
+
+	def __repr__(self):
+		print(f'<view_id={self.view_id} user_id={self.user_id} view_name={self.view_name}>')
+
+class ViewHotel(db.Model):
+	"""View Hotel Model - the hotels associated with a view."""
+
+	view_id = db.Column(db.Integer, db.ForeignKey('view.view_id'), primary_key=True,)
+	hotel_id = db.Column(db.Integer, db.ForeignKey('hotels.hotel_id'), primary_key=True,)
 
 ######################################################
 # Helper functions
@@ -56,12 +86,13 @@ def connect_to_db(app):
 	"""Connect the database to our Flask app"""
 
 	# Configure to use our database
-	app.config['SQLALCHEMY_DATABASE-URI'] = 'postgres:///hotels'
+	app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres:///hotels'
 	app.config['SQLALCHEMY_ECHO'] = False
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    db.app = app
-    db.init_app(app)
+	app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+	db.app = app
+	db.init_app(app)
 
 if __name__ == '__main__':
 
 	init_app()
+	db.create_all()
