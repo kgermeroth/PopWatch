c82d631a030ec2398fa7c34bcd0ec1d139d2e712
Kristin Germeroth <kristin.germeroth@gmail.com>
2019-08-19 16:48:23 -0700

    removed all commented out beautiful soup code

--- web-scraping.py
+++ web-scraping.py
@@ -1,4 +1,4 @@
-import webbrowser, bs4, requests, re, csv, time
+import webbrowser, requests, re, csv
 from datetime import datetime
 from model import *
 
@@ -46,73 +46,6 @@ def write_html_to_file(full_page, filename):
 
 	return filepath
 
-
-# def convert_html_file(file_to_save_to):
-# 	"""Takes in html file and coverts it to Beautiful Soup object"""
-
-# 	# open the html file and get data out
-# 	with open(file_to_save_to, 'r') as f:
-# 		the_text = f.read()
-
-# 	# turn html into a beautifulsoup object
-# 	soup_object = bs4.BeautifulSoup(the_text, features="html.parser")
-
-# 	return soup_object
-
-
-# def get_data_out_of_soup(soup_object):
-# 	"""Take beautiful soup object and pull out releveant data"""
-
-# 	# pull html with rank information out of soup object
-# 	rankhtml = soup_object.find('span', class_='header_popularity')
-
-# 	# have to account for any missing class names when html doesn't pull properly
-# 	if rankhtml is None:
-# 		num_hotels = ''
-# 		rank = ''
-
-# 	else:
-# 		# Match the rank # and number of hotels in the rank_text
-# 		rankhtml = rankhtml.text
-# 		match_obj = re.search(r'\#(\d+) of (\d+)', rankhtml)
-# 		rank = int(match_obj.group(1))
-# 		num_hotels = int(match_obj.group(2))
-
-# 	# Pull html with avg review score out of soup object and convert to float
-# 	avgscore = soup_object.find('span', class_='hotels-hotel-review-about-with-photos-Reviews__overallRating--vElGA')
-
-# 	# have to account for any missing class names when html doesn't pull properly
-# 	if avgscore is None:
-# 		avgscore = ''
-
-# 	else:
-# 		avgscore = float(avgscore.text)
-
-
-# 	# parse out review count and find num of reviews with regex
-# 	reviewcount_html = soup_object.find('span', class_='reviewCount')
-
-# 	# have to account for any missing class names when html doesn't pull properly
-# 	if reviewcount_html is None:
-# 		reviewcount = ''
-
-# 	else:
-# 		reviewcount_html = reviewcount_html.text
-# 		match_obj = re.search(r'([0-9,]+)', reviewcount_html)
-# 		reviewcount = match_obj.group(1)
-
-# 		# need to account for commas before converting to int
-# 		if len(reviewcount) < 4:															#less than 1,000 reviews
-# 			reviewcount = int(reviewcount)
-
-# 		elif len(reviewcount) > 3 and len(reviewcount) < 8:									#1,000 - 999,999 reviews
-# 			reviewcount = int((reviewcount[:-4] + reviewcount[-3:]))
-
-# 		else:																				#more 999,999 reviews
-# 			reviewcount = int((reviewcount[:-8] + reviewcount[-7:-4] + reviewcount[-3:]))
-
-
-# 	return (rank, avgscore, num_hotels, reviewcount)
 
 def use_regex(file_to_save_to):
 	"""Opens HTML file and uses Regex to get information"""
