a1aaa1094585b02596b606ae62fb9e3876055193
Kristin Germeroth <kristin.germeroth@gmail.com>
2019-09-16 13:54:33 -0700

    reverted debug stuff to see if 502 error resolves

--- server.py
+++ server.py
@@ -283,10 +283,10 @@ def log_user_off():
 
 if __name__ == '__main__':
 
-    # app.debug = True
+    app.debug = True
 
     # make sure templates, etc. are not cached in debug mode
-    # app.jinja_env.auto_reload = app.debug
+    app.jinja_env.auto_reload = app.debug
 
     connect_to_db(app)
 
